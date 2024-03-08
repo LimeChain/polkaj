2a681be2b0aee7e23671770a8aacde22192af42e
David Petrov <david.petrov@limechain.tech>
2024-03-08 15:21:10 +0200

    refactor!: remove `appendU64` from `TranscriptData`
    dubbed too inconvenient as a public API method since java doesn't provide a u64 type, so the correct representation is left to the caller

--- polkaj-schnorrkel/src/main/java/io/emeraldpay/polkaj/merlin/TranscriptData.java
+++ polkaj-schnorrkel/src/main/java/io/emeraldpay/polkaj/merlin/TranscriptData.java
@@ -1,6 +1,5 @@
 package io.emeraldpay.polkaj.merlin;
 
-import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
@@ -8,11 +7,16 @@ import java.util.ArrayList;
  * A container class to simply hold all the necessary input data (label + messages) to construct the actual
  * transcript on Rust's side. Think of this as "the bag of currently necessary arguments".
  * The main idea of this class is to be easily portable to Rust using JNI.
- * It makes no sense on its own on the Java side alone.
+ * It has no usage on its own on the Java side alone.
  */
+@SuppressWarnings({"MismatchedQueryAndUpdateOfCollection", "FieldCanBeLocal"})
 public class TranscriptData {
-    // INTENTIONALITY: Those fields being of type ArrayList is essential for the JNI mappings
+    // INTENTIONALITY: Those fields being of type ArrayList is essential for the JNI mappings on the Rust side
 
+    // HACK:
+    //  Semantically, the internal representation of the domainSeparationLabel should be a single byte[]
+    //  Due to an unresolved issue with robusta (the rust lib) though,
+    //  the field mapping from byte[] doesn't work as expected, so this is the current workaround.
     private final ArrayList<byte[]> domainSeparationLabel;
 
     private final ArrayList<byte[]> labels;
@@ -25,41 +29,31 @@ public class TranscriptData {
         this.messages = new ArrayList<>();
     }
 
+    /**
+     * Appends an ASCII encoded string message to the transcript with an ASCII encoded string label.
+     * @param label the ASCII encoded label
+     * @param message the ASCII encoded message
+     */
     public void appendMessage(String label, String message) {
-        appendMessage(label.getBytes(StandardCharsets.US_ASCII), message.getBytes(StandardCharsets.US_ASCII));
-    }
-
-    public void appendMessage(byte[] label, byte[] message) {
-        labels.add(label);
-        messages.add(message);
+        appendMessage(label, message.getBytes(StandardCharsets.US_ASCII));
     }
 
     /**
-     * Appends a message with an u64 body.
-     * <br>
-     * NOTE: only the last 8 bytes of the BigInteger's byte[]
-     *       representation will be taken and reversed into little-endian order
-     * @param label the message label
-     * @param x the u64 to append
+     * Appends a message to the transcript with an ASCII encoded string label.
+     * @param label the ASCII encoded label
+     * @param message the actual message (content)
      */
-    // TODO:
-    //  Maybe just remove this method and leave it up to the user...
-    //  We can't guarantee good enough type safety and obeying the representational contract is left to the developer,
-    //  which could lead to issues.
-    public void appendAsU64(byte[] label, BigInteger x) {
-        appendMessage(label, encodeBigIntAsU64LittleEndian(x));
+    public void appendMessage(String label, byte[] message) {
+        appendMessage(label.getBytes(StandardCharsets.US_ASCII), message);
     }
 
-    private static byte[] encodeBigIntAsU64LittleEndian(BigInteger x) {
-        final int length = 8;
-        byte[] byteArray = x.toByteArray();
-        byte[] littleEndian = new byte[length];
-
-        final int upperBound = Math.min(length, byteArray.length);
-        for (int i = 0; i < upperBound; i++) {
-            littleEndian[i] = byteArray[byteArray.length - 1 - i];
-        }
-
-        return littleEndian;
+    /**
+     * Appends a message to the transcript.
+     * @param label the label of the message
+     * @param message the actual message (content)
+     */
+    public void appendMessage(byte[] label, byte[] message) {
+        labels.add(label);
+        messages.add(message);
     }
 }
